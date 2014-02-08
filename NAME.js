1ff803b61dff32c3816d5a45a9a04ef6bb0779e7
khilnani <nik@khilnani.org>
2014-02-08 12:46:13 -0500

    mongodb

--- NAME.js
+++ NAME.js
@@ -1,16 +1,26 @@
 #!/usr/bin/env node
 
+// vars ========================
+
 var express = require('express');
 var routes = require('./app/routes');
 var http = require('http');
 var path = require('path');
 var less = require('less-middleware');
 var hb = require('express3-handlebars');
+var mongodb = require('mongodb');
+
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID
 
 var app = express();
 var config = require('./config/env.json')[app.get('env')];
 
-// all environments
+// setup ========================
+
 app.use(express.favicon());
 app.use(express.json());
 app.use(express.urlencoded());
@@ -19,34 +29,38 @@ app.use(app.router);
 
 app.use(less({ src: path.join(__dirname, 'public') }));
 app.set('views', path.join(__dirname, 'app/views'));
-// UN/COMMENT if using jade views
-// app.set('view engine', 'jade');
-// END
-// UN/COMMENT if using handlebars views
-var hbs = hb.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-//END
+var hbs = hb.create({});  // handlebars 
+app.engine('handlebars', hbs.engine); // handlebars 
+app.set('view engine', 'handlebars'); // handlebars 
+// app.set('view engine', 'jade');  // jade
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/libs', express.static(__dirname + '/bower_components'));
 app.use('/libs/jade', express.static(__dirname + '/node_modules/jade'));
 
+// configuration =================
 
 app.set('port', config.port);
 app.use(express.logger('dev'));
 
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-  }));
-}
 
-// production only
-if ('production' === app.get('env')) {
-  app.use(express.errorHandler());
-}
+var db = new Db('test', new Server('127.0.0.1', 27017, {auto_reconnect: true}, {w: 1, safe: false}));
+db.open(function(err, db){
+  console.log('Connected to mongodb ');
+  var col = db.collection('test');
+  var c = {x: { $gt: 3}};
+  col.find(c).toArray( function(e, items) {
+    console.log( items );
+  } );
+});
+
+
+app.use(express.errorHandler({
+  dumpExceptions: config.dumpExceptions,
+  showStack: config.showStack
+}));
+
+// Routes =================
 
 app.get('/', routes.index);
 
