39a4088da1573d8ff4751661c4918dd211eb226f
Nik <nik@khilnani.org>
2014-02-23 22:22:42 -0500

    Added locale

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,3 +3,7 @@ var locale =  require('./../locale').default();
 exports.index = function (req, res) {
   res.render('index', { name: locale.global.title, description: locale.global.description });
 };
+
+exports.locale = function (req, res) {
+  res.json(locale);
+};
