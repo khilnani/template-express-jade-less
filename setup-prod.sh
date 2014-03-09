672f38001a070e46dfb975c3847f6ef52c6565e5
khilnani <nik@khilnani.org>
2014-03-09 15:25:32 -0400

    Updated

--- setup-prod.sh
+++ setup-prod.sh
@@ -1,13 +1,16 @@
-#!/bin/sh -x
+#!/bin/sh
 
+if [ ! -d node_modules ]; then
 
-## Globals
+  ## Globals
+  npm install -g forever
+  npm install -g handlebars
+  npm install -g gulp
 
-npm install -g forever
-npm install -g handlebars
-npm install -g gulp
-
-## Project Specific
- 
-npm install
+  ## Project Specific
+   
+  npm install
+else
+  echo "node_modules already exists"
+fi
 
