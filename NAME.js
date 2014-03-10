cc3254f7a239de6e440eb4e4c242eb7fd6162ec4
Nik <nik@khilnani.org>
2014-03-09 20:15:12 -0400

    Update NAME.js

--- NAME.js
+++ NAME.js
@@ -2,18 +2,13 @@
 
 // vars ========================
 
+var log = require('dysf.utils').logger;
 var express = require('express');
 var routes = require('./app/routes');
 var http = require('http');
 var path = require('path');
 var less = require('less-middleware');
 var hb = require('express3-handlebars');
-
-//var Db = require('mongodb').Db;
-//var Connection = require('mongodb').Connection;
-//var Server = require('mongodb').Server;
-//var BSON = require('mongodb').BSON;
-//var ObjectID = require('mongodb').ObjectID;
 
 var app = express();
 var config = require('./config/env.json')[app.get('env')];
@@ -38,20 +33,9 @@ app.use('/libs/jade', express.static(__dirname + '/node_modules/jade'));
 
 // configuration =================
 
+log.setLogLevel( config.logLevel );
 app.set('port', config.port);
 app.use(express.logger('dev'));
-
-/*
-var db = new Db('test', new Server('127.0.0.1', 27017, {auto_reconnect: true}, {w: 1, safe: false}));
-db.open(function(err, db){
-  console.log('Connected to mongodb ');
-  var col = db.collection('test');
-  var c = {x: { $gt: 3}};
-  col.find(c).toArray( function(e, items) {
-    console.log( items );
-  } );
-});
-*/
 
 app.use(express.errorHandler({
   dumpExceptions: config.dumpExceptions,
@@ -66,6 +50,6 @@ app.get('/api/locale', routes.locale);
 // Start server =================
 
 http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-  console.log('ENV: ' + app.get('env'));
+  log.system('Express server listening on port ' + app.get('port'));
+  log.info('Environment: ' + app.get('env'));
 });
